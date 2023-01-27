786584920007784d28179c23e9e652376ba0da35
Cristian Venegas <cristian.venegas@materoceano.com>
2023-01-26 21:58:09 -0300

    add: 04 unit Test

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,8 +1,9 @@
 const request = require("supertest");
 const server = require("../index");
+const cafes = require("../cafes.json");
 
 describe("Operaciones CRUD de cafes", () => {
-  it("devuelve un status code 200 y el tipo de dato recibido es un arreglo con por lo menos 1 objeto", async () => {
+  it("01 - Devuelve un status code 200 y el tipo de dato recibido es un arreglo con por lo menos 1 objeto", async () => {
     const response = await request(server).get("/cafes").send();
     const status = response.statusCode;
     expect(status).toBe(200);
@@ -10,20 +11,31 @@ describe("Operaciones CRUD de cafes", () => {
     expect(response).toHaveProperty(Object.keys(response)[0]);
   });
 
-  it("Comprueba que se obtiene un código 404 al intentar eliminar un café", async () => {
+  it("02 - Comprueba que se obtiene un código 404 al intentar eliminar un café", async () => {
     const response = await request(server).delete("/cafes/6").send();
     const status = response.statusCode;
     expect(status).toBe(400);
   });
 
-  it("Prueba que la ruta POST /cafes agrega un nuevo café y devuelve un código 201", async () => {
-    const newCafe =   {
-      "id": 5,
-      "nombre": "frappuccino"
-    }
-    
+  it("03 - Prueba que la ruta POST /cafes agrega un nuevo café y devuelve un código 201", async () => {
+    const newCafe = {
+      id: 5,
+      nombre: "frappuccino",
+    };
+
     const response = await request(server).post("/cafes").send(newCafe);
     const status = response.statusCode;
     expect(status).toBe(201);
+  });
+
+  it("04 - Prueba que la ruta PUT /cafes devuelve un status code 400 si intentas actualizar un café enviando un id en los parámetros que sea diferente al id dentro del payload", async () => {
+    const cafe = {
+      id: 1,
+      nombre: "frappuccino Venti",
+    };
+
+    const response = await request(server).put("/cafes/2").send(cafe);
+    const status = response.statusCode;
+    expect(status).toBe(400);
   });
 });
